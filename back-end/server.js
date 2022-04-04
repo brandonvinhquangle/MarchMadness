8bd09dfe59a5828485b5da61f38bd3c6ba5838b0
Brandon Vinh Le <brandonvinhquangle@gmail.com>
2022-04-03 23:04:16 -0600

    Edit server.js

--- back-end/server.js
+++ back-end/server.js
@@ -17,6 +17,26 @@ mongoose.connect('mongodb+srv://CS-260:Password@cluster0.mdzpn.mongodb.net/museu
   useNewUrlParser: true
 });
 
+/* Multer */
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+/* Data for the APIs */
+// Creates a scheme for items in the museum: a title and a path to an image.
+const itemSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  path: String,
+});
+
+// Creates a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+
 /* REST APIs */
 // Get a list of all of the items in the museum.
 app.get('/api/items', async (req, res) => {
@@ -45,6 +65,18 @@ app.post('/api/items', async (req, res) => {
   }
 });
 
+// Uploads a photo. Uses the multer middleware for the upload and then returns
+// the path where the photo is stored in the file system.
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
 // Deletes an item in the museum: takes an id
 app.delete('/api/items/:id', async (req, res) => {
   try {
@@ -69,37 +101,6 @@ app.put('/api/items/:id', async (req, res) => {
     console.log(error);
     res.sendStatus(500);
   }
-});
-
-/* Multer */
-const multer = require('multer')
-const upload = multer({
-  dest: '../front-end/public/images/',
-  limits: {
-    fileSize: 10000000
-  }
-});
-
-// Creates a scheme for items in the museum: a title and a path to an image.
-const itemSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  path: String,
-});
-
-// Creates a model for items in the museum.
-const Item = mongoose.model('Item', itemSchema);
-
-// Uploads a photo. Uses the multer middleware for the upload and then returns
-// the path where the photo is stored in the file system.
-app.post('/api/photos', upload.single('photo'), async (req, res) => {
-  // Just a safety check
-  if (!req.file) {
-    return res.sendStatus(400);
-  }
-  res.send({
-    path: "/images/" + req.file.filename
-  });
 });
 
 const port = process.env.PORT || 3000;
